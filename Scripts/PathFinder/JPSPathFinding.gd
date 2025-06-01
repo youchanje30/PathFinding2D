4bdb18dcdea65a7295e95c75f59cc3291cc14a69
psk <77984774+youchanje30@users.noreply.github.com>
2025-06-02 00:01:35 +0900

    JPS 개선 작업 중
    JS로 구현된 NeverMoveDiagonally라는 코드를 발견했다.
    
    해당 코드를 기반으로 JPS를 구현하려고 한다.

--- Scripts/PathFinder/JPSPathFinding.gd
+++ Scripts/PathFinder/JPSPathFinding.gd
@@ -19,33 +19,41 @@ func heuristic(start : Vector2i, end : Vector2i) -> int:
 	return abs(start.x - end.x) + abs(start.y - end.y)
 
 func jump(board : BoardData, pos : Vector2i, dir : Vector2i)->Vector2i:
-	while true:
-		if pos == end: return pos
-		if is_stop: return -Vector2i.ONE
+	await get_tree().create_timer(0.0001).timeout 
+	if pos == end: return pos
+	if is_stop: return -Vector2i.ONE
 
-		#await get_tree().create_timer(0.0001).timeout 
+	var next = pos + dir
+	if not board.can_visit(next.x, next.y): return -Vector2i.ONE
+	board.visit(next.x, next.y)
+	parents[next.y][next.x] = pos
+	dp[next.y][next.x] = dp[pos.y][pos.x] + 1
 
-		var next = pos + dir
-		if not board.can_visit(next.x, next.y): return -Vector2i.ONE
-		board.visit(next.x, next.y)
-		parents[next.y][next.x] = pos
-		dp[next.y][next.x] = dp[pos.y][pos.x] + 1
+	# 수평 이동일 때 (dx != 0)
+	if dir.x != 0:
+		if (board.can_visit(pos.x, pos.y-1) and not board.can_visit(pos.x-dir.x, pos.y-1)) or \
+		 (board.can_visit(pos.x, pos.y+1) and not board.can_visit(pos.x-dir.x, pos.y+1)):
+			return pos
 
-		for check_dir in move_list:
-			if check_dir == dir or check_dir == -dir: continue
-			var check = pos + check_dir
-
-			if not board.is_valid_position(check.x, check.y): continue
-			if not board.is_wall(check.x, check.y): continue
-
-			var corner = check + dir
-			if not board.can_visit(corner.x, corner.y): continue
-
+	# 수직 이동일 때 (dy != 0)
+	elif dir.y != 0:
+		# 좌우로 강제 이웃이 있는지 확인
+		var left_pos = Vector2i(next.x - 1, next.y)
+		var right_pos = Vector2i(next.x + 1, next.y)
+		var prev_left = Vector2i(next.x - 1, pos.y)
+		var prev_right = Vector2i(next.x + 1, pos.y)
+		
+		if (board.can_visit(left_pos.x, left_pos.y) and not board.can_visit(prev_left.x, prev_left.y)) or (board.can_visit(right_pos.x, right_pos.y) and not board.can_visit(prev_right.x, prev_right.y)):
 			return next
-	
-		pos = next
-	return -Vector2i.ONE
-	
+		
+		# 수직 이동시 수평 방향의 점프 포인트 확인
+		var left_jump = await jump(board, pos, Vector2i(-1, 0))
+		if left_jump != -Vector2i.ONE: return next
+
+		var right_jump = await jump(board, pos, Vector2i(1, 0))
+		if right_jump != -Vector2i.ONE: return next
+		
+	return await jump(board, pos+dir, dir)
 
 func init(max_x, max_y) -> void:
 	super.init(max_x, max_y)
@@ -64,13 +72,12 @@ func path_find(board_data : BoardData, _start : Vector2i, _end : Vector2i):
 	board_data.visit(start.x, start.y)
 
 	while not pq.empty() and not found:
-		
 		var current = pq.top(); pq.pop()
 		var cost = current[0]
 		var pos = current[1]
 
 		if cost - heuristic(pos, end) > dp[pos.y][pos.x]: continue
-		#await get_tree().create_timer(0.0001).timeout 
+		# await get_tree().create_timer(0.0001).timeout 
 		cost = dp[pos.y][pos.x]
 
 		for add in move_list:
@@ -82,61 +89,10 @@ func path_find(board_data : BoardData, _start : Vector2i, _end : Vector2i):
 
 			# 누적 거리(g) 계산
 			var g = dp[pos.y][pos.x] + (jump_point - pos).length()
-			if dp[jump_point.y][jump_point.x] < g: print(11111); continue
+			if dp[jump_point.y][jump_point.x] < g: continue
 
 			dp[jump_point.y][jump_point.x] = g
 			pq.push([g + heuristic(jump_point, end), jump_point])
-
-		var check_dir = [
-			[Vector2i(1, 1), Vector2i(1, 0), Vector2i(0, 1)],
-			[Vector2i(-1, 1), Vector2i(-1, 0), Vector2i(0, 1)],
-			[Vector2i(1, -1), Vector2i(1, 0), Vector2i(0, -1)],
-			[Vector2i(-1, -1), Vector2i(-1, 0), Vector2i(0, -1)]
-		]
-		
-		#for check_list in check_dir:
-			#var target = check_list[0]
-			#var check1 = check_list[1]
-			#var check2 = check_list[2]
-#
-			#var target_pos = pos + target
-			#if not board_data.can_visit(target_pos.x, target_pos.y): continue
-#
-			#var check1_pos = pos + check1
-			#var check2_pos = pos + check2
-			#var is_change = false
-#
-			#if not board_data.is_wall(check1_pos.x, check1_pos.y):
-				#parents[target_pos.y][target_pos.x] = check1_pos; is_change = true
-			#if not board_data.is_wall(check2_pos.x, check2_pos.y):
-				#parents[target_pos.y][target_pos.x] = check2_pos; is_change = true
-			#
-			#if is_change:
-				#if target_pos == end: found = true; break
-				#board_data.visit(target_pos.x, target_pos.y)
-				#pq.push([dp[pos.y][pos.x] + 2 + heuristic(target_pos, end), target_pos])
-				#dp[target_pos.y][target_pos.x] = dp[pos.y][pos.x] + 2
-		
-		for check_list in check_dir:
-			var target = check_list[0]
-			var target_pos = pos + target
-			if not board_data.can_visit(target_pos.x, target_pos.y): continue
-			
-			var min_dir = Vector2i.ZERO
-			for move in move_list:
-				var check = target_pos + move
-				if not board_data.is_valid_position(check.x, check.y): continue
-				if dp[check.y][check.x] + 1 > dp[target_pos.y][target_pos.x]: continue
-				dp[target_pos.y][target_pos.x] = dp[check.y][check.x] + 1
-				min_dir = move
-			if min_dir == Vector2i.ZERO: continue
-			parents[target_pos.y][target_pos.x] = target_pos + min_dir
-			if target_pos == end: found = true; break
-			pq.push([dp[target_pos.y][target_pos.x] + heuristic(target_pos, end), target_pos])
-			board_data.visit(target_pos.x, target_pos.y)
-			
-			
-			
 
 		if found: break
 
